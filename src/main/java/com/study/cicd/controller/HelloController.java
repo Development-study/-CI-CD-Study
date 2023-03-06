56711907777ce67b6fff760734639b72fa2563bd
yongbin <rhdydqls1024@naver.com>
2023-03-06 13:57:06 +0900

    :memo: :: Add HelloController

---
+++ src/main/java/com/study/cicd/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.study.cicd.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+
+}
